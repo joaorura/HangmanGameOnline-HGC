cd6af6b13d214495e47dc194c492b33617b0208b
João Messias Lima Pereira <jmlp@ic.ufal.br>
2020-01-11 00:09:04 -0300

    ...

--- client/front/menu.py
+++ client/front/menu.py
@@ -15,14 +15,38 @@ class Menu(tk.Frame):
         self.button_enter = tk.Button(self, text="Enter Room", command=self._button_enter)
         self.button_enter.pack()
 
-    def _button_exit(self):
-        self.intergame.exit_client()
-
     def _button_create(self):
-        pass
+        win = tk.Toplevel()
+
+        title = tk.Label(win, text="Create Room")
+        title.pack()
+
+        name = tk.Label(win, text="Name: ")
+        name.pack()
+
+        name_input = tk.Text(win)
+        name_input.pack()
+
+        password = tk.Label(win, text="Password: ")
+        password.pack()
+
+        password_input = tk.Text(win)
+        password_input.pack()
+
+        commit_button = tk.Button(win, text="Ok", command=self.intergame.create_room(name_input, password_input))
+        commit_button.pack()
 
     def _button_enter(self):
-        pass
+        win = tk.Toplevel()
 
-    def change_menu(self, param):
-        pass
+        title = tk.Label(win, text="Enter Room")
+        title.pack()
+
+        _id = tk.Label(win, text="Code: ")
+        _id.pack()
+
+        id_input = tk.Text(win)
+        id_input.pack()
+
+        commit_button = tk.Button(win, text="Ok", command=self.intergame.enter_room(id_input))
+        commit_button.pack()
--- client/interface/server_to_game/inter_game.py
+++ client/interface/server_to_game/inter_game.py
@@ -2,17 +2,30 @@ from front.menu import Menu
 from interface.game_to_server.game_server import GameServer
 import tkinter as tk
 
-from interface.server_to_game.window_process import WindowProcess
-
 
 class InterGame:
     def __init__(self, queue_send, queue_receive):
         self.game_to_server = GameServer(queue_send, queue_receive)
         self.window = None
 
-    def enter_room(self, id_room):
-        self.window.change_to_room(id_room, self.game_to_server.enter_room)
-        pass
+    def create_room(self, name_input, password_input):
+        def f():
+            name = name_input.get("1.0", "end-1c")
+            password = password_input.get("1.0", "end-1c")
+
+            send = {
+
+            }
+            print(name, password)
+
+        return f
+
+    def enter_room(self, id_room_input):
+        def f():
+            id_room = id_room_input.get("1.0", "end-1c")
+            print(id_room)
+
+        return f
 
     def exit_room(self):
         self.window.change_menu((
@@ -21,16 +34,8 @@ class InterGame:
             self.game_to_server.exit_room
         ))
 
-        pass
-
     def start(self):
         aux = tk.Tk()
         self.window = Menu(self, aux)
         self.window.pack()
-        self.window.change_menu((
-            self.game_to_server.create_room,
-            self.game_to_server.enter_room,
-            self.game_to_server.exit_room
-        ))
-
         self.window.mainloop()
